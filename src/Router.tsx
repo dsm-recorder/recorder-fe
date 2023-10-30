c60c9a04addf6c95d5b7b59228c9a05f778b4ce3
ensnif060305 <jmsun0305@gmail.com>
2023-10-30 18:06:17 +0900

    fix: LandingPage로 이름 변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import LandingPage from './page/LandinPage';
+import LandingPage from './page/LandingPage';
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path='/' element={<LandingPage />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   );
