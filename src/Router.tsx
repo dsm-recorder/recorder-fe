3026feff089539ccd6c9dc7d6f4f0150740ab9ca
ensnif060305 <jmsun0305@gmail.com>
2023-10-30 18:12:08 +0900

    feat: ProjectRegisterPage 라우터 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import LandingPage from './page/LandingPage';
+import ProjectRegisterPage from './page/ProjectRegisterPage';
 
 export const Router = () => {
   return (
@@ -8,6 +9,7 @@ export const Router = () => {
       <Header />
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/project-register" element={<ProjectRegisterPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/page/ProjectRegisterPage.tsx
@@ -0,0 +1,9 @@
+export const ProjectRegisterPage = () => {
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default ProjectRegisterPage;
