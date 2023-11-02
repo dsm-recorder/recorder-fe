4a050beb06f3c20b5f7e93ce64a710b8e4d3776d
ensnif060305 <jmsun0305@gmail.com>
2023-11-02 20:17:03 +0900

    feat: 이미지 업로드 api 연동

---
+++ src/api/image/index.ts
@@ -0,0 +1,27 @@
+import { useMutation } from '@tanstack/react-query';
+import { instance } from '../axios';
+import { IImgResponse } from './type';
+
+const ROUTER = 'images';
+
+export const PostImage = () => {
+  const response = async (param: File) => {
+    const formData = new FormData();
+    formData.append('image', param);
+
+    const config = {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    };
+
+    return (await instance.post<IImgResponse>(`${ROUTER}`, formData, config))
+      .data.url;
+  };
+
+  return useMutation(response, {
+    onError: (e) => {
+      alert(e);
+    },
+  });
+};
