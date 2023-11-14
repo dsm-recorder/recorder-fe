9d1d772acab1ef96c1f21e551de7a953187cb4c6
ensnif060305 <jmsun0305@gmail.com>
2023-11-14 09:09:08 +0900

    feat: api 연동

---
+++ src/page/ProjectAbout.tsx
@@ -0,0 +1,86 @@
+import styled from 'styled-components';
+import ProjectDescription from '@/components/ProjectAbout/ProjectDescription';
+import TodoList from '@/components/ProjectAbout/TodoList';
+import PRList from '@/components/ProjectAbout/PR';
+import { GetTodayReport } from '@/api/daily-reports';
+import { useLocation } from 'react-router-dom';
+import { GetPRReport } from '@/api/pr-records';
+import { ProjectType } from '@/api/projects/type';
+import { HStack, VStack } from '@/components/Stack';
+
+const ProjectAbout = () => {
+  const location = useLocation();
+  const state = location.state as ProjectType;
+
+  const { data: todoLists } = GetTodayReport(state.id);
+  const { data: prLists } = GetPRReport(state.id);
+
+  return (
+    <Container>
+      <PageWrapper>
+        <ProjectInfoWrapper>
+          <HStack justify='space-between'>
+            <ProjectLogoImg alt='projectLogoImg' src={state.logoImageUrl} />
+            <VStack>
+              <ProjectName>{state.name}</ProjectName>
+              <ProjectCreateAt>{state.createdAt}</ProjectCreateAt>
+            </VStack>
+          </HStack>
+        </ProjectInfoWrapper>
+        <ProjectDescription description={state.description} />
+        <TodoList todos={todoLists?.todos ?? []} />
+        <PRList prRecords={prLists?.prRecords ?? []} />
+      </PageWrapper>
+    </Container>
+  );
+};
+
+export default ProjectAbout;
+
+const ProjectInfoWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  gap: 30px;
+`;
+
+const Container = styled.div`
+  display: flex;
+  padding: 180px 240px;
+  flex-direction: column;
+  min-height: 100vh;
+  background-color: ${({ theme }) => theme.colors.gray[30]};
+`;
+
+const PageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  min-width: 700px;
+  padding: 60px 120px;
+  background-color: ${({ theme }) => theme.colors.gray[10]};
+  align-items: flex-start;
+  border-radius: 15px;
+  flex-direction: column;
+  gap: 60px;
+`;
+
+const ProjectLogoImg = styled.img`
+  width: 120px;
+  height: 120px;
+  border-radius: 10px;
+  background: lightgray 50% / cover no-repeat;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  background-color: ${({ theme }) => theme.colors.gray[60]};
+`;
+
+const ProjectName = styled.div`
+  color: ${({ theme }) => theme.colors.gray[100]};
+  font-size: 36px;
+  font-weight: 700;
+`;
+
+const ProjectCreateAt = styled.div`
+  color: ${({ theme }) => theme.colors.gray[60]};
+  font-size: 24px;
+  font-weight: 600;
+`;
