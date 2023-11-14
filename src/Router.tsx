8c43820546a16c13309a5e8649d00dacd2bbd17f
ensnif060305 <jmsun0305@gmail.com>
2023-11-14 09:10:03 +0900

    fix: 라우터 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import Header from '@/components/Header';
 import LandingPage from '@/page/LandingPage';
 import ProjectRegisterPage from '@/page/ProjectRegisterPage';
 import MyPage from '@/page/MyPage';
+import ProjectAbout from '@/page/ProjectAbout'
 import NotFoundPage from '@/page/NotFoundPage';
 import Footer from '@/components/Footer';
 
@@ -14,6 +15,7 @@ export const Router = () => {
         <Route path='/' element={<LandingPage />} />
         <Route path='/project-register' element={<ProjectRegisterPage />} />
         <Route path='/mypage' element={<MyPage />} />
+        <Route path='/project-about/:id' element={<ProjectAbout />} />
         <Route path='/*' element={<NotFoundPage />} />
       </Routes>
       <Footer />
