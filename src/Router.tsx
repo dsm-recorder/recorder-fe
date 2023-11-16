942d6aea7141806b44a6d758c2906c53e28f4a83
ensnif060305 <jmsun0305@gmail.com>
2023-11-16 22:04:44 +0900

    fix: router 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,9 +3,9 @@ import Header from '@/components/Header';
 import LandingPage from '@/page/LandingPage';
 import ProjectRegisterPage from '@/page/ProjectRegisterPage';
 import MyPage from '@/page/MyPage';
-import ProjectAbout from '@/page/ProjectAbout'
 import NotFoundPage from '@/page/NotFoundPage';
 import Footer from '@/components/Footer';
+import ProjectWritingPage from './page/ProjectWritingPage';
 
 export const Router = () => {
   return (
@@ -15,7 +15,7 @@ export const Router = () => {
         <Route path='/' element={<LandingPage />} />
         <Route path='/project-register' element={<ProjectRegisterPage />} />
         <Route path='/mypage' element={<MyPage />} />
-        <Route path='/project-about/:id' element={<ProjectAbout />} />
+        <Route path='/project-writing/:id' element={<ProjectWritingPage />} />
         <Route path='/*' element={<NotFoundPage />} />
       </Routes>
       <Footer />
